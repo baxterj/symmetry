73ea3c8e0d7ebd290cc88514f39a2982bf6e8f27
James Baxter <baxterj405@gmail.com>
2016-01-02 19:49:29 +0000

    move js into closure

--- js/paper-script.js
+++ js/paper-script.js
@@ -1,23 +1,68 @@
-var path;
+(function(window) {
 
-// The mouse has to drag at least 20pt
-// before the next drag event is fired:
-tool.minDistance = 20;
+    paper.install(window);
 
-function onMouseDown(event) {
-    if (path) {
-        path.selected = false;
-    };
-    path = new Path();
-    path.strokeColor = 'black';
-    path.fullySelected = true;
-}
+    config = {
+        path: undefined // The path we are drawing
+    }
 
-function onMouseDrag(event) {
-    path.add(event.point);
-}
+    window.onload = function() {
+        // Get a reference to the canvas object
+        var canvas = document.getElementById('myCanvas');
+        // Create an empty project and a view for the canvas:
+        paper.setup(canvas);
+        // Create a Paper.js Path to draw a line into it:
+        config.path = new paper.Path();
+       
 
-function onMouseUp(event) {
-    path.selected = false;
-    path.smooth();
-}
+        paper.view.onFrame = function(event) {
+            paper.view.draw();
+        }
+
+        createTools();
+    }
+
+
+    function createTools() {
+        var pen = new Tool();
+        // The mouse has to drag at least 20pt
+        // before the next drag event is fired:
+        pen.minDistance = 2;
+
+        pen.onMouseDown = function(event) {
+            if (config.path) {
+                config.path.selected = false;
+            };
+            config.path = new Path();
+            config.path.strokeColor = 'black';
+            config.path.fullySelected = true;
+        }
+
+        pen.onMouseDrag = function(event) {
+            config.path.add(event.point);
+        }
+
+        pen.onMouseUp = function(event) {
+            config.path.selected = false;
+            config.path.smooth();
+            config.path.simplify();
+            // config.path.selected = true;
+        }
+    }
+
+    window.Symmetry = {
+        // Do something
+    }
+
+})(this);
+
+
+
+
+
+
+
+
+
+
+
